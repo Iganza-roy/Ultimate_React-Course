5d2cfc6b74e421b7b6b80131dd717a33aacd6e0d
Iganza-roy <iganzaroy55@gmail.com>
2024-12-13 13:32:05 +0300

    Fix typo in FAQ entry text for Accordion component

--- Accordion/src/App.js
+++ Accordion/src/App.js
@@ -12,7 +12,7 @@ const faqs = [
   },
   {
     title: 'Do you ship to countries outside the EU?',
-    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+    text: 'Exceptu velit laboru, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
   },
 ];
 
